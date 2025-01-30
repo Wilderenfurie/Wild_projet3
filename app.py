36671baf6df67e52766531ceb15441984f7ec788
Mainell <mainell.wj@gmail.com>
2025-01-30 15:49:59 +0100

    Mise à jour biblio & print df

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import nltk
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('vader_lexicon')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 #historique quotidien du prix de vente a la cloture de crude oil wti
 def oil_historical_price_api():
@@ -136,3 +145,111 @@ def oil_key_indicators():
 
 
 print(oil_key_indicators())
+
+
+# Dataframe avec données numériques
+df_prix_vente_alpha = oil_historical_price_api()
+print(df_prix_vente_alpha)
+
+df_yahoo=oil_historical_price_v2()
+print(df_yahoo)
+
+df_production = oil_field_production_api()
+print(df_production)
+
+df_stock = oil_commercial_stock_api()
+print(df_stock)
+
+df_supply = oil_days_supply_api()
+print(df_supply)
+
+
+#Web scraping & sentiment analysis
+# Création d'une fonction qui analyse le contenu de plusieurs sites web
+def sitesweb_analyse_sentiment(url: str):
+    navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
+    html = requests.get(url, headers={'User-Agent': navigator})
+    soup = BeautifulSoup(html.text, 'html.parser')
+    for unwanted_tag in soup(['script','style','iframe','ins','advertissement','ads','ad','footer', 'header','a','menu','nav', 'aside','form', 'input', 'select']):
+        unwanted_tag.decompose()
+    text = soup.get_text()
+    sid = SentimentIntensityAnalyzer() #Bibliothèque vaderSentiment
+    sentiment_score = sid.polarity_scores(text)
+    if sentiment_score['compound'] >= 0:
+        return 'positif'
+    else:
+        return 'négatif'
+
+
+# Création d'une fonction qui analyse la tendance globale avec l'API News
+def apinews_tendance_globale():
+    url = f"https://newsapi.org/v2/everything?q=Crude+Oil+WTI&apiKey=0ef44c92338042508e7b2ed9afc37e5b"
+    response = requests.get(url)
+    articles = response.json().get("articles", [])
+    analyzer = SentimentIntensityAnalyzer() #Bibliothèque vaderSentiment
+    positive_count = 0
+    negative_count = 0
+    num_articles = 0
+    for article in articles:
+        title = article.get("title", "Titre non disponible")
+        description = article.get("description", "")
+        content = article.get("content", "")
+        text_to_analyze = description if description else content
+        if text_to_analyze:
+            sentiment_score = analyzer.polarity_scores(text_to_analyze)
+            compound_score = sentiment_score['compound']
+            if compound_score > 0.1:
+                positive_count += 1
+            elif compound_score < -0.1:
+                negative_count += 1
+            num_articles += 1
+    if num_articles > 0:
+        if positive_count > negative_count:
+            return "positive"
+        elif negative_count > positive_count:
+            return "négative"
+        else:
+            return "équilibrée"
+    else:
+        return "aucun article"
+
+# Création d'une fonction qui fait le bilan des colonnes d'un dataframe
+def bilan_global(row):
+    positive_count = sum([1 for sentiment in row[1:] if sentiment == "positif"])
+    negative_count = sum([1 for sentiment in row[1:] if sentiment == "négatif"])
+    if positive_count > negative_count:
+        return "positif majoritaire"
+    elif negative_count > positive_count:
+        return "négatif majoritaire"
+    else:
+        return "bilan équilibré"
+
+# Création d'une fonction pour créer le dataframe lié au Sentiment Analysis
+def add_to_dataframe(urls):
+    global df_sentiment
+    sentiments = []
+    date = datetime.now().strftime("%Y-%m-%d")
+    for url in urls:
+        sentiment = sitesweb_analyse_sentiment(url)
+        sentiments.append(sentiment)
+    column_names = ["Date", "EIA-TodayinEnergy", "OILPRICE-WorldNews", "CNBC-CrudeOilWTI",
+                    "TRADINGECONOMICS-CrudeOilWTI", "INVESTING-CrudeOilWTI"]
+    new_row = [date] + sentiments
+    global_trend = apinews_tendance_globale()
+    new_row.append(global_trend)
+    df_new_row = pd.DataFrame([new_row], columns = column_names + ["API News"])
+    df_new_row["Bilan global"] = df_new_row.apply(lambda row: bilan_global(row), axis=1)
+    df_sentiment = pd.concat([df_sentiment, df_new_row], ignore_index = True)
+
+# Application des fonctions précédentes
+df_sentiment = pd.DataFrame()
+urls = [
+    "https://www.eia.gov/todayinenergy/",
+    "https://oilprice.com/Latest-Energy-News/World-News/",
+    "https://www.cnbc.com/quotes/@CL.1",
+    "https://tradingeconomics.com/commodity/crude-oil",
+    "https://www.investing.com/commodities/crude-oil-news"
+]
+
+add_to_dataframe(urls)
+print(df_sentiment)
